76bcd7db21157fa0a52c182db2748c416e754479
fo-create <65010852+fo-create@users.noreply.github.com>
2024-01-29 09:07:52 +0800

    Update base_net.py

--- base_net.py
+++ base_net.py
@@ -12,7 +12,7 @@ from torch.autograd import Function
 from math import sqrt
 
 import random
-
+这是一个定义了卷积块的 PyTorch 模块 ConvBlock。这个模块接受一些卷积层的参数，并提供了选择性的标准化（Normalization）和激活（Activation）。
 class ConvBlock(torch.nn.Module):
     def __init__(self, input_size, output_size, kernel_size=3, stride=1, padding=1, bias=True, activation='prelu', norm=None):
         super(ConvBlock, self).__init__()
@@ -37,6 +37,7 @@ class ConvBlock(torch.nn.Module):
             self.act = torch.nn.Sigmoid()
 
     def forward(self, x):
+         # 根据是否有标准化层应用卷积
         if self.norm is not None:
             out = self.bn(self.conv(x))
         else:
@@ -80,7 +81,7 @@ class DeconvBlock(torch.nn.Module):
             return self.act(out)
         else:
             return out
-
+这个模块类似于卷积块，但使用了反卷积层（转置卷积层）。
 
 class ConvLayer(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
@@ -93,8 +94,10 @@ class ConvLayer(nn.Module):
 #         out = self.reflection_pad(x)
         out = self.conv2d(x)
         return out
+这个模块的作用是对输入张量应用卷积操作。
 
-
+实现了上采样卷积层。这个模块接受输入通道数、输出通道数、卷积核大小和步长作为参数，并使用 nn.ConvTranspose2d 实现反卷积（转置卷积）操作。
+这个模块的作用是对输入张量应用反卷积操作，实现上采样。
 class UpsampleConvLayer(torch.nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride):
       super(UpsampleConvLayer, self).__init__()
@@ -104,7 +107,7 @@ class UpsampleConvLayer(torch.nn.Module):
         out = self.conv2d(x_idwt)
         return out
 
-
+这个模块接受通道数作为参数，并包含两个卷积层和一个 ReLU 激活函数，用于学习残差映射。残差块的设计有助于训练深层网络，允许模型学习残差而不是直接学习映射。
 class ResidualBlock(torch.nn.Module):
     def __init__(self, channels):
         super(ResidualBlock, self).__init__()
@@ -137,13 +140,13 @@ def init_conv(conv, glu=True):
 class EqualLR:
     def __init__(self, name):
         self.name = name
-
+算等权重初始化后的权重。
     def compute_weight(self, module):
         weight = getattr(module, self.name + '_orig')
         fan_in = weight.data.size(1) * weight.data[0][0].numel()
 
         return weight * sqrt(2 / fan_in)
-
+apply 方法用于将等权重初始化应用到模块的指定参数上。它通过注册 EqualLR 类的实例为模块添加一个 forward pre-hook，该 hook 将在前向传播之前调用。
     @staticmethod
     def apply(module, name):
         fn = EqualLR(name)
@@ -154,7 +157,7 @@ class EqualLR:
         module.register_forward_pre_hook(fn)
 
         return fn
-
+__call__ 方法是 hook 的具体实现，在前向传播之前调用。它使用 compute_weight 方法来计算等权重初始化后的权重，并将其设置为模块的参数。
     def __call__(self, module, input):
         weight = self.compute_weight(module)
         setattr(module, self.name, weight)
